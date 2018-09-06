7991427736e36ba1a2f23fae896c3f589d598426
Guilherme Raimondi <raimondi.it@gmail.com>
2018-09-05 23:21:29 -0300

    Using Dto pattern to return users

--- src/main/java/com/raimondiguilherme/workshopmongo/resource/UserResource.java
+++ src/main/java/com/raimondiguilherme/workshopmongo/resource/UserResource.java
@@ -1,6 +1,7 @@
 package com.raimondiguilherme.workshopmongo.resource;
 
 import com.raimondiguilherme.workshopmongo.domain.User;
+import com.raimondiguilherme.workshopmongo.dto.UserDTO;
 import com.raimondiguilherme.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController // recurso rest
 @RequestMapping(value="/users") // caminho do endpoint
@@ -20,10 +22,11 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET) // pra dizer que este metodo vai ser o endpoint rest do caminho /users
-    public ResponseEntity<List<User>> findAll(){ // RE vai encapsular a estrutura necessaria pra retornar resposta http com possiveis cabecalhos, erros
+    public ResponseEntity<List<UserDTO>> findAll(){ // RE vai encapsular a estrutura necessaria pra retornar resposta http com possiveis cabecalhos, erros
 
         List<User> users = service.findAllInDatabase();
-        return ResponseEntity.ok().body(users); // .ok:   vai instanciar a RE ja com o codigo de resposta http que retornou com sucesso
+        List<UserDTO> usersDTO = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(usersDTO); // .ok:   vai instanciar a RE ja com o codigo de resposta http que retornou com sucesso
                                                 // .body: corpo da resposta
     }
 
