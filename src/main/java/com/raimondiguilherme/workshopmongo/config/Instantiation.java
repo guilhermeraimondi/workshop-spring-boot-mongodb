5e0d05ff3a04400676f42d653b82835a362f0b63
ZUP - Guilherme Raimondi <zup-guilhermeraimondi@MacBook-Pro-de-Guilherme.local>
2018-09-08 13:54:17 -0300

    Projection of author data with DTO

--- src/main/java/com/raimondiguilherme/workshopmongo/config/Instantiation.java
+++ src/main/java/com/raimondiguilherme/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.raimondiguilherme.workshopmongo.config;
 
 import com.raimondiguilherme.workshopmongo.domain.Post;
 import com.raimondiguilherme.workshopmongo.domain.User;
+import com.raimondiguilherme.workshopmongo.dto.AuthorDTO;
 import com.raimondiguilherme.workshopmongo.repository.PostRepository;
 import com.raimondiguilherme.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,10 +33,11 @@ public class Instantiation implements CommandLineRunner { // instanciar todos os
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, sdf.parse("11/03/2018"), "Partiu viagem!", "Vou viajar para SP, abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia familia", "Acordei disposta hoje", maria);
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        // eh necessario persistir os objetos User antes de relacionar, pois se nao o db nao tera criado o id para eles e nos post author id estara null
+
+        Post post1 = new Post(null, sdf.parse("11/03/2018"), "Partiu viagem!", "Vou viajar para SP, abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia familia", "Acordei disposta hoje", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
